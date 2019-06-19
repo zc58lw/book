99ae2d02ce379c71bd0a918bbc8b51ec58004f6b
HP <xu.yiyang@eisoo.com>
2019-06-19 09:45:33 +0800

    xyy:Arrow function of class

--- js/pages/ImgesDemo.js
+++ js/pages/ImgesDemo.js
@@ -7,16 +7,19 @@ import {Link} from 'react-router-dom'
 
 // React component
 class ImgesDemo extends Component {
-  render() {
-    return (<div>
-        <img src={require('../../images/1.png')}/>
-        <img src={require('../../images/2.jpg')}/>
-        <img src={require('../../images/index/3.jpg')}/>
-        <div className={'backgroung_img'}>
-        
-        </div>
-      </div>)
-  }
+    func=()=>{
+    
+    }
+    render() {
+        return (<div>
+            <img src={require('../../images/1.png')}/>
+            <img src={require('../../images/2.jpg')}/>
+            <img src={require('../../images/index/3.jpg')}/>
+            <div className={'backgroung_img'}>
+            
+            </div>
+        </div>)
+    }
 }
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
                     loader: "babel-loader",
                     options: {
                         presets: [
-                            "es2015", "react"
+                            "es2015", "react","stage-0"
                         ]
                     }
                 },
